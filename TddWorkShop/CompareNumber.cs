2a7b79b615e59c6a4cfdd61f67e178f231de38f0
hdwangTW <hdwang@thoughtworks.com>
2016-06-22 22:24:26 +0800

    stand up the project

---
+++ TddWorkShop/CompareNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDWorkShop
+{
+    public class CompareNumber
+    {
+        public static string compare(int inputNumber, int randomNumber)
+        {
+            return default(string);
+        }
+    }
+}
